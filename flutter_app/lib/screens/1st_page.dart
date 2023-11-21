06ed80d670587505b9f414a285dbd9e647391251
Anuradha jayasanke <anuradhajekanayake@gmail.com>
2023-11-21 23:57:34 +0530

    11

--- flutter_app/lib/screens/1st_page.dart
+++ flutter_app/lib/screens/1st_page.dart
@@ -29,7 +29,7 @@ class firstpage extends StatelessWidget {
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
                   gradient: const LinearGradient(
-                      colors: [Colors.purple, Colors.purpleAccent])),
+                      colors: [Colors.black, Colors.purpleAccent])),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -57,6 +57,79 @@ class firstpage extends StatelessWidget {
                 ],
               ),
             ),
+            const SizedBox(
+              width: 20,
+              height: 20,
+            ),
+            Container(
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  gradient: const LinearGradient(
+                      colors: [Colors.orange, Colors.deepOrange])),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.login,
+                    color: Colors.white,
+                    size: 50,
+                  ),
+                  const Text(
+                    "Allready Have Account",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const Text(
+                    "click butten for LogIn",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  FilledButton(
+                    onPressed: () {},
+                    child: const Text("LogIn"),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+              width: 20,
+            ),
+            Container(
+              width: double.infinity,
+              height: 300,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  gradient: const LinearGradient(
+                      colors: [Colors.black38, Colors.blueAccent])),
+              child: const Column(children: [
+                Text(
+                  "Wellcome To THAPRO",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 20,
+                ),
+                Text(
+                  "THAPRO coin is the leading crypto curruncy that offers secure and efficient finantial solutions in the digitel curruncy world whether you are new to cryptocurruncy or an experienced ,thapro coin has you coverd",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ]),
+            )
           ],
         ),
       ),
